60d3d5ece450f415f5c2673b83b4a5192ea1f8a6
LamarElite <65634223+LamarElite@users.noreply.github.com>
2020-07-16 22:11:28 -0400

    Add files via upload

---
+++ src/main/java/com/lamar/springproject/ProjectSpringBootLgApplication.java
@@ -0,0 +1,13 @@
+package com.lamar.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringBootLgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringBootLgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lamar/springproject/ProjectSpringBootLgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamar.springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringBootLgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
